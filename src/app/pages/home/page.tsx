c872934fe2d5f025a35d406257aa55cb5d26bb38
parkoon <devparkoon@gmail.com>
2025-11-07 10:46:59 +0900

    feat: 임시 제거

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -2,12 +2,11 @@
 
 import { PageLayout } from '@/components/layouts/page-layout'
 import { VideoFeeds } from '@/features/video/components/video-feeds'
-import { useAppCloseConfirm } from '@/hooks/use-app-close-confirm'
 
 import { FloatingKeyWithBottomSheet } from './_components/floating-key-with-bottom-sheet'
 
 const Home = () => {
-  useAppCloseConfirm()
+  // useAppCloseConfirm()
   return (
     <PageLayout>
       <VideoFeeds />
